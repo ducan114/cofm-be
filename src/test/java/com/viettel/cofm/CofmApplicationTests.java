e8b495a41129a3aa2f20f76684537ee7d27cabad
ducan114 <ducan11042001@gmail.com>
2025-09-18 16:36:40 +0000

    chore: initialize spring boot
    Install maven.
    
    Add base dependencies

---
+++ src/test/java/com/viettel/cofm/CofmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viettel.cofm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
